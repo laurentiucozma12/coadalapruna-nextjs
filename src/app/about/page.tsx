8a6ad692db35db7e1e4637dcf26986752d243609
Lau <laurentiucozma12@gmail.com>
2024-07-25 18:11:03 +0300

    udpate

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Navbar } from '../components/Navbar'
+
+export default function About() {
+    return (
+        <>
+            <Navbar /> <main>About page</main>
+        </>
+    )
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Navbar } from '../components/Navbar'
+
+export default function Contact() {
+    return (
+        <>
+            <Navbar /> <main>Contact page</main>
+        </>
+    )
+}
