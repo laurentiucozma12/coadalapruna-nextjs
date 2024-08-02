b9635998f72b83c300e8c2812ed0437355696aec
Lau <laurentiucozma12@gmail.com>
2024-08-02 11:04:02 +0300

    custom hook for navbar added, renaming of contact component, CardAd added, and small updates for texts

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -10,11 +10,6 @@ export const Contact = () => {
         <>
             <AlertComponent />
             <div className="m-2 cursor-pointer text-white">
-                {/*     
-                margin top is 75px (67px + 8px),
-                67px, because height of navbar is 67px 
-                8px because the rest of  the elements have a m-4 which is 16px
-                */}
                 <h2
                     className="text-md rounded bg-black p-4 text-center font-extrabold shadow-sm duration-500 sm:text-2xl"
                     onClick={() => copyToClipboard('0764237914')}
